502a0510a4104557705f094240d4d07b84623584
Nick D <ndiasio97@gmail.com>
2017-09-24 14:55:16 -0400

    Changed variable names to make code  easier to follow

--- filter_by_color.py
+++ filter_by_color.py
@@ -6,49 +6,50 @@ import imutils
 #ap = argparse.ArgumentParser(description= "this is where the image/videa source is")
 #ap.add_argument('-i', "--image", help = "path to the video feed")
 #args = vars(ap.parse_args())
+#sourceImage= cv2.imread(args["image"])
 
-    #sourcePixel= cv2.imread(args["image"])
-sourcePixel = cv2.imread("RBY_targets.jpg")
+sourceImage = cv2.imread("RBY_Targets.jpg")
 #convert pixel ratio to resize image
-ratio = sourcePixel.shape[0] / 300.0
-orig = sourcePixel.copy()
-sourcePixel = imutils.resize(sourcePixel, height = 300)
-gray = cv2.cvtColor(sourcePixel, cv2.COLOR_BGR2GRAY)
-edge = cv2.Canny(sourcePixel, 100,200)
+ratio = sourceImage.shape[0] / 300.0
+orig = sourceImage.copy()
+sourceImage = imutils.resize(sourceImage, height = 300)
+edgesOnlyImage = cv2.Canny(sourceImage, 150,250)
 
 #show image with edges only to test if it is working
-#cv2.imshow("edgish", edge)
-#cv2.waitKey(0)
+cv2.imshow("edgish", edgesOnlyImage)
+cv2.waitKey(0)
 
-#create an array containing the three desired color ranges
-boundaries =[
-                ([65, 5, 0], [125, 65, 35]),
-                ([45, 180, 200], [75, 210, 235]),
-                ([55, 55, 180], [115, 105, 240]),
+#create an array containing the three desired GBR color ranges: blue,yellow, red
+colorBoundaries =[
+                ([45, 0, 0], [145, 95, 55]),
+                ([25, 160, 180], [95, 230, 255]),
+                ([50, 50, 175], [125, 115, 250]),
                 ]
-    #loop through the ranges
-for (lower, upper) in boundaries:
-        #openCV needs numpy arrays, cannot omit this
-    lower = np.array(lower, dtype = "uint8")
-    upper = np.array(upper, dtype = "uint8")
-        #turns pixels in range white, else blaxk
-    colorMask = cv2.inRange(sourcePixel, lower, upper)
+#loop through the ranges
+for (lowerBound, upperBound) in colorBoundaries:
+    #openCV needs numpy arrays, cannot omit this
+    lower = np.array(lowerBound, dtype = "uint8")
+    upper = np.array(upperBound, dtype = "uint8")
+    #turns pixels in range white, else black
+    colorMask = cv2.inRange(sourceImage, lower, upper)
 
-        #finds union of image and mask
-    outputArray = cv2.bitwise_and(sourcePixel, sourcePixel, mask = colorMask)
-    blurredOutputArray = cv2.bilateralFilter(outputArray, 11, 17, 17)
-    edgyPhoto = cv2.Canny(blurredOutputArray, 100, 200)
+    #finds union of image and mask
+    maskedSourceImage = cv2.bitwise_and(sourceImage, sourceImage, mask = colorMask)
+    #blurs photo slightly to reduce false positives
+    blurredMaskImage = cv2.bilateralFilter(maskedSourceImage, 11, 17, 17)
+    edgyPhoto = cv2.Canny(blurredMaskImage, 100, 200)
 
-    #now we will find the actual edges in the photo
-    (_, contours, _) = cv2.findContours(edgyPhoto.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #now we will find the actual edges in the photo by looking for contours
+    (_, contours, _) = cv2.findContours(edgyPhoto.copy(), cv2.RETR_TREE,
+                        cv2.CHAIN_APPROX_SIMPLE)
     #contours = sorted(contours, key = cv2.contourArea, reverse = True)
     screenContour = None #initialize variable
 
     for cntr in contours:
-            #creates a n approximation of the contours
+        #creates an approximation of the contours
         peri = cv2.arcLength(cntr, True)
         approx = cv2.approxPolyDP(cntr, 0.01 * peri, True)
-            #checks how many points in each contour, four points => square
+        #checks how many points in each contour, four points => rectangle
         if len(approx) >= 4 and len(approx) <= 6:
             (x, y, w, h) = cv2.boundingRect(approx)
             aspectRatio = w / float(h)
@@ -60,20 +61,20 @@ for (lower, upper) in boundaries:
 
             #check to see if the width, height, and solidity and aspect ratio
             #fall within the appropriate bounds
-            keepDims = w > 30 and h > 30
+            keepDims = w > 25 and h > 25
             keepSolidity = solidity > 0.9
             keepAspectRatio = aspectRatio >= 0.8 and aspectRatio <= 1.2
 
             #check to see if all the tests were passed
             if keepDims and keepSolidity and keepAspectRatio:
                 #draw an outline around the target and update the status
-                cv2.drawContours(blurredOutputArray, [approx], -1, (0, 0, 255), 4)
+                cv2.drawContours(blurredMaskImage, [approx], -1, (255, 255, 0), 4)
                 status = "Target aquired"
                 print(status)
             else:
                 print("Not Aquired", keepDims, keepSolidity, keepAspectRatio)
 
     #show the filtered images
-    cv2.imshow("images", np.hstack([sourcePixel, blurredOutputArray]))
-    #cv2.imshow("Aquired Target", outputArray)
+    cv2.imshow("images", np.hstack([sourceImage, blurredMaskImage]))
+    #cv2.imshow("Aquired Target", maskedSourceImage)
     cv2.waitKey(0)
